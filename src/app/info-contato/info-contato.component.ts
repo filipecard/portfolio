ff901a4a2a863a518d32b085644d146684551266
Filipe Cardoso <filipecardosooliveira@hotmail.com>
2023-01-30 17:47:43 -0300

    mod: finish info-contato component

--- src/app/info-contato/info-contato.component.ts
+++ src/app/info-contato/info-contato.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class InfoContatoComponent {
 
+
+  redirectToWhatsapp(){
+    window.open("https://wa.me/5586981346155", "_blank");
+  }
+
+  redirectToLinkedin(){
+    window.open("https://www.linkedin.com/in/filipecarolidev/", "_blank");
+  }
+
+  redirectToGithub(){
+    window.open("https://github.com/filipecard", "_blank");
+  }
+
+
 }
+
